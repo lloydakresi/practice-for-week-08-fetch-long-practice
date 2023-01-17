6a385de320c8cddbb3ed098a9758141798920d8a
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-17 11:16:29 +0000

    completed...

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,12 +6,34 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+let url = "/products"
+let header = {"Content-Type": "`application/x-www-form-urlencoded`"}
+let method = "POST";
+let body = new URLSearchParams({
+    name: "Carribean Delight Coffee",
+    description: "Made by Manatee Coffee",
+    price: 11.99,
+    category: "grocery"
+})
+
+fetch("/products", {
+    method: method,
+    headers: header,
+    body: body
+})
+
 
 
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
+.then(response =>{
+
+    console.log(response.status);
+    console.log(response.headers.get("Content-Type"));
+    console.log(response.)
+})
 
 
 
